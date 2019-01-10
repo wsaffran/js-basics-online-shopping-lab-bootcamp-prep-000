facbf5a42f6372b84f7fe1d9403cc8f974b4972a
William Saffran <willh.saffran@gmail.com>
2019-01-10 18:55:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,9 +64,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (cardNumber) {
-    for (let i = 0; i < cart.length; i++)
-      cart[i].pop()
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
+    cart.pop()
   }
   else
     return `Sorry, we don't have a credit card on file for you.`
