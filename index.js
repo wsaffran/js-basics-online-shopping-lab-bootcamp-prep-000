8f30bc8901b3d788c0205be1b7a8cbbf4689a7da
William Saffran <willh.saffran@gmail.com>
2019-01-10 18:42:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].itemName === item)
+      cart.splice(i, 1)
+  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
