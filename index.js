5f800000af6b37ea2bde4279ac753610dc96611e
William Saffran <willh.saffran@gmail.com>
2019-01-10 19:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -69,4 +69,6 @@ function placeOrder(cardNumber) {
   else
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
   cart = []
+  cart.length = 0
+  cart.pop()
 }
