d31e87df532b71066fb8d5639b33010ef0496708
William Saffran <willh.saffran@gmail.com>
2019-01-10 18:35:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,34 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var price = Math.ceil(Math.random()*100)
+ var newItem = {itemName: item, itemPrice: price}
+ cart.push(newItem)
+ return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var cartLength = cart.length
+  if (cartLength === 0)
+    return `Your shopping cart is empty.`
+
+  var newArray = []
+    for (let i = 0; i < cartLength; i++) {
+      newArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+    }
+
+  var intro = `In your cart, you have`
+  if (cartLength === 1)
+    return `${intro} ${newArray[0]}.`
+  if (cartLength === 2)
+    return `${intro} ${newArray[0]}, and ${newArray[1]}.`
+  if (cartLength > 2) {
+    for (let i = 0; i < cartLength-1; i++) {
+        intro = `${intro} ${newArray[i]},`
+    }
+    intro = `${intro} and ${newArray[cartLength-1]}.`
+    return intro
+  }
 }
 
 function total() {
